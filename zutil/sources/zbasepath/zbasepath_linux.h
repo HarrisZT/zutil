c817aea5381d7544aae5a59f8b0ee1566440c8f1
HarrisZT <84755887+HarrisZT@users.noreply.github.com>
2021-06-03 12:16:10 -0400

    Update zbasepath_linux.h

--- zutil/sources/zbasepath/zbasepath_linux.h
+++ zutil/sources/zbasepath/zbasepath_linux.h
@@ -36,190 +36,44 @@ freely, subject to the following restrictions:
 #    define __STDC_FORMAT_MACROS
 #  endif
 #  include <inttypes.h> 
+
 #  if defined(__ANDROID__) || defined(ANDROID)
 #    include <fcntl.h>
 #    include <sys/mman.h>
 #    include <unistd.h>
-#  endif
- 
-#  if defined(__sun)
-#    define _PROC_SELF_EXE  "/proc/self/path/a.out"
-#    define _PROC_SELF_MAPS "/proc/self/map"
-#  else
-#    define _PROC_SELF_EXE  "/proc/self/exe"
-#    define _PROC_SELF_MAPS "/proc/self/maps"
 #  endif 
-#  define _PROC_SELF_MAPS_RETRY 5  
- 
-#define _SCAN_FORMAT \
-  "%" PRIx64 "-%" PRIx64 " %s %" PRIx64 " %x:%x %u %s\n"
- 
-// local file header signature found
-#define _LOCAL_HDR_SIG 0x04034B50UL
+#  if defined(__sun)
+#    define _PROC_SELF_EXE  "/proc/self/path/a.out" 
+#  else
+#    define _PROC_SELF_EXE  "/proc/self/exe" 
+#  endif  
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-
 	   
-	static int Linux_GetExePath(
-		char* _cPathOut,
-		int   _iCapacity, 
-		int*  _iDirnameLength) {
+	static Int32
+	LinuxGetBasePath(
+		_Inout_opt_ Char* _out,
+		_In_        Int32 _len) {
 
-		char  cBuffer[PATH_MAX];
-		char* cResolved = NULL;
-		int   iLength = -1;
+		Char  cBuffer[Z_MAXPATH];
+		Char* cResolved = NULL;
+		Int32 iLength   = -1;
 
 		for (;;) {
 			cResolved = realpath(_PROC_SELF_EXE, cBuffer);
 			if (!cResolved) {
 				break;
 			}
-			iLength = (int)strlen(resolved);
-			if (iLength <= _iCapacity) {
-				memcpy(_cPathOut, cResolved, iLength);
-
-				if (_iDirnameLength) {
-					int i; 
-					for (i = iLength - 1; i >= 0; --i) {
-						if (_cPathOut[i] == '/') {
-							*_iDirnameLength = i;
-							break;
-						}
-					}
-				}
-			}
+			iLength = (Int32)strlen(resolved);
+			if (iLength <= _len) 
+				memcpy(_out, cResolved, iLength); 
 			break;
 		}
 		return iLength;
-	}
-
- 
-	static int Linux_GetModPath(
-		char* _cPathOut, 
-		int   _iCapacity, 
-		int*  _iDirnameLength) {
-
-		int   iLength = -1;
-		FILE* maps    = NULL;
-
-		for (int r = 0; r < _PROC_SELF_MAPS_RETRY; ++r) {
-			maps = fopen(_PROC_SELF_MAPS, "r");
-			if (!maps) { 
-				break;
-			}
-			for (;;) {
-				char     perms[5];
-				char     path[PATH_MAX];
-				char     buffer[PATH_MAX < 1024 ? 1024 : PATH_MAX];
-				uint64_t low, high; 
-				uint64_t offset;
-				uint32_t major, minor; 
-				uint32_t inode;
-
-				if (!fgets(buffer, sizeof(buffer), maps))
-					break;
-
-				if (sscanf(
-					buffer, 
-					_SCAN_FORMAT, 
-					&low, &high,
-					perms, 
-					&offset, 
-					&major, &minor, 
-					&inode, 
-					path) == 8) {
-
-					uint64_t addr = (uintptr_t)F2D_ReturnAddress();
-					if (low <= addr && addr <= high) {
-						char* resolved; 
-						resolved = realpath(path, buffer);
-						if (!resolved) {
-							break;
-						}
-						iLength = (int)strlen(resolved);
-
-					#if defined(__ANDROID__) || defined(ANDROID)
-						if (iLength > 4
-							&& buffer[iLength - 1] == 'k'
-							&& buffer[iLength - 2] == 'p'
-							&& buffer[iLength - 3] == 'a'
-							&& buffer[iLength - 4] == '.') {
-
-							int fd = open(path, O_RDONLY);
-							if (fd == -1) {
-								iLength = -1; // retry
-								break;
-							} 
-							char* begin = (char*)mmap(
-								0, offset, 
-								PROT_READ, MAP_SHARED, 
-								fd, 0);
-
-							if (begin == MAP_FAILED) {
-								close(fd);
-								iLength = -1; // retry
-								break;
-							} 
-							// minimum size of local file header:
-							char* p = begin + offset - 30; 
-
-							while (p >= begin) {// scan backwards 
-								if (*((uint32_t*)p) == _LOCAL_HDR_SIG) {
-									uint16_t length_ = *((uint16_t*)(p + 26));
-
-									if (iLength + 2 + length_ < 
-										(int)sizeof(buffer)) {
-
-										memcpy(
-											&buffer[iLength], "!/", 2);
-										memcpy(
-											&buffer[iLength + 2], 
-											p + 30, length_);
-
-										iLength += 2 + length_;
-									} 
-									break;
-								} 
-								--p;
-							} 
-							munmap(begin, offset);
-							close(fd);
-						}
-					#endif
-
-						if (iLength <= _iCapacity) {
-							memcpy(_cPathOut, resolved, iLength);
-
-							if (_iDirnameLength) {
-								int i; 
-								for (i = iLength - 1; i >= 0; --i) {
-									if (_cPathOut[i] == '/') {
-										*_iDirnameLength = i;
-										break;
-									}
-								}
-							}
-						} 
-						break;
-					}
-				}
-			}
-			fclose(maps);
-			maps = NULL; 
-
-			if (iLength != -1) {
-				break;
-			}
-		}
-		if (maps) {
-			fclose(maps);
-		}
-		return iLength;
-	}
-
+	} 
 
 #ifdef __cplusplus
 }
